41cd0fdeb1ecff0078df1fc6a98c104038b6a3c3
diljahonsoatboyev <jahongir55005500@gmail.com>
2026-04-15 16:47:23 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,10 +41,10 @@ async def search_songs(query):
     'default_search': 'ytsearch10',
     'noplaylist': True,
     'quiet': True,
-    'nocheckcertificate': True, # Qo'shing
-    'geo_bypass': True,          # Qo'shing
-    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' # Qo'shing
+    # Quyidagi qatorni qo'shing:
+    'user_agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1'
 }
+
 
     with YoutubeDL(ydl_opts) as ydl:
         info = await asyncio.to_thread(ydl.extract_info, query, download=False)
